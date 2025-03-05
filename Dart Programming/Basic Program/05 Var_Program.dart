2f0269702ebf51c340a33a7b2e7d822612f6e0df
LadAkanksha <akankshalad2003@gmail.com>
2025-03-05 19:39:11 +0530

    Basics

---
+++ Dart Programming/Basic Program/05 Var_Program.dart
@@ -0,0 +1,17 @@
+void main() {
+  var differ = "Flutter";
+  print(differ);
+  print(differ.runtimeType);
+
+  differ = "Android";
+  print(differ);
+  print(differ.runtimeType);
+
+  //differ = 121.67;
+  /*A value of type 'double' can't be assigned to a variable of type 'String'.
+ Try changing the type of the variable, or casting the right-hand type to 'String'.*/
+
+  /*
+  In Var type, Once the type is initialized, It remains fixed and can't altered.
+  */
+}

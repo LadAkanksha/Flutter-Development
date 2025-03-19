b6dc1c4757f6f6aaf4e3c8829dcfa49d7721b257
LadAkanksha <akankshalad2003@gmail.com>
2025-03-19 13:08:09 +0530

    Basic

---
+++ Dart Programs/Basic Program/03 Double_Program.dart
@@ -0,0 +1,5 @@
+void main() {
+  num Percenatge = 91.80; //num stores only Integer / Double Value.
+  print(Percenatge);
+  print(Percenatge.runtimeType); //runtimeType specify type of identifier.
+}

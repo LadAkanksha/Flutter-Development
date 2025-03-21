d060832f307658ef97b75dbd95408e74642cb5c5
LadAkanksha <akankshalad2003@gmail.com>
2025-03-21 19:20:39 +0530

    Control Statement

---
+++ Dart Programs/Control Statements/01 if statement.dart
@@ -0,0 +1,8 @@
+void main() {
+  int year = 2003;
+
+  if (year % 4 == 0) {
+    print("$year is Leap Year");
+  }
+  print("---Thank You---");
+}
---
+++ Dart Programs/Control Statements/02 if_else statement.dart
@@ -0,0 +1,9 @@
+void main() {
+  int year = 2028;
+
+  if (year % 4 == 0) {
+    print("$year is Leap Year");
+  } else {
+    print("$year is not Leap Year");
+  }
+}
